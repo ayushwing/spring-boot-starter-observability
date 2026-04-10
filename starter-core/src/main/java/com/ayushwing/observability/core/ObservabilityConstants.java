caa60181460d3b436ce36d0c37bf6490d1730ff0
ayushwing <sayushsoni@gmail.com>
2026-04-10 23:37:18 +0530

    feat: add correlation ID generation and propagation

--- starter-core/src/main/java/com/ayushwing/observability/core/ObservabilityConstants.java
+++ starter-core/src/main/java/com/ayushwing/observability/core/ObservabilityConstants.java
@@ -8,6 +8,7 @@ public final class ObservabilityConstants {
     public static final String TRACE_ID_KEY = "traceId";
     public static final String SPAN_ID_KEY = "spanId";
     public static final String REQUEST_ID_KEY = "requestId";
+    public static final String CORRELATION_ID_KEY = "correlationId";
     public static final String CORRELATION_ID_HEADER = "X-Correlation-Id";
     public static final String CONFIG_PREFIX = "observability";
 
